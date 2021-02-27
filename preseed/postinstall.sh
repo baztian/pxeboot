e77913352c4c981a195bccabba7d631c45755295
Bastian Bowe <bastian.dev@gmail.com>
2021-02-27 14:56:00 +0100

    Install etckeeper

--- preseed/postinstall.sh
+++ preseed/postinstall.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
+apt install -y etckeeper
 sed -i 's/^managed=false/managed=true/' /etc/NetworkManager/NetworkManager.conf
 
 sed -i 's/^XKBOPTIONS=.*/XKBOPTIONS="compose:menu"/' /etc/default/keyboard
 
 apt install -y openssh-server
 sed -i 's/Port 22/Port 12345/' /etc/ssh/sshd_config
-apt-get -y install git
+
+apt install -y git
 apt-add-repository -y ppa:ansible/ansible
-apt-get update
-apt-get -y install ansible
+apt update
+apt install -y ansible
 cat > /etc/ansible/ansible.cfg <<HERE
 
 [defaults]
 interpreter_python = /usr/bin/python3
 nocows = 1
 HERE
+etckeeper commit "ansible configuration"
